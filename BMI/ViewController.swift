78cacf5330cddcf023b23ec95cb09668b58daffe
hellooooworld <wook9947@naver.com>
2016-04-13 15:02:00 +0900

    160413-01

--- BMI/ViewController.swift
+++ BMI/ViewController.swift
@@ -9,6 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
+    
+    
+    var weight : Double?
+    var height : Double?
+    
+    var bmi: Double? {
+        get {
+            if (weight != nil) && (height != nil){
+                return weight! / (height! * height!)
+            } else {
+                return nil
+            }
+        }
+    }
+    
 
     @IBOutlet weak var bmiLabel: UILabel!
     
@@ -31,10 +46,42 @@ class ViewController: UIViewController, UITextFieldDelegate {
         // tell the textField to resign as the first responder
         // tell the textField to stop receiving keyboard input
         textField.resignFirstResponder()
-        
         return true
     }
-
-
-}
+    
+    //Update UI
+    func updateUI() {
+        if let b = self.bmi {
+            self.bmiLabel.text = String (format: "%4.1f", b)
+        }
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        
+        guard let txt: String = textField.text else{
+            return
+        }
+        
+        func conv(numString: String) -> Double? {
+            let result : Double? = NSNumberFormatter().numberFromString(numString)?.doubleValue
+            return result
+        }// End of conv
+        
+        if let txt = textField.text {
+            switch(textField){
+            case heightTextField:
+                self.height = conv(txt)
+            case weightTextFleid:
+                self.weight = conv(txt)
+            default:
+                print(" Something Wrong")
+            }// End of switch
+            
+            updateUI()
+            
+        }// End of if
+        
+    }// End of func textFieldDidEndEditing
+    
+}// End of whole function
 
